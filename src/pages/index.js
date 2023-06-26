ec988a03810d1fe3424602414b682d9228010ba2
Sebas Z <juanzambrano97@hotmail.com>
2023-06-26 13:01:51 -0500

    Added all our new blog code for Gastby setup

---
+++ src/pages/index.js
@@ -0,0 +1,67 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`;
+
+const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
+
+export default ({ data }) => (
+  <Layout>
+    <Seo title="Home" />
+    <div>
+      <h1>JLFreedom's Thoughts</h1>
+      <h4>{ data.allMarkdownRemark.totalCount }</h4>
+      {
+        data.allMarkdownRemark.edges.map(({node}) => (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>{ node.frontmatter.title } - { node.frontmatter.date }</BlogTitle>
+            </BlogLink>
+            <p>{ node.excerpt }</p>
+          </div>
+        ))
+      }
+    </div>
+  </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
+            date
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
---
+++ src/pages/page-3.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+
+import Layout from "../components/layout"
+
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLO FROM THE 3rd Page</h1>
+    <p>Welcome to page 3</p>
+  </Layout>
+)
+
+
+
+export default ThirdPage
